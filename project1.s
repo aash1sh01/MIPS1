e2a84f4fd129b4367e2a4d74134dd572c13e5b17
unknown <a.adhikari6960@gmail.com>
2020-10-10 11:39:20 +0545

    Here we are reading the input string as an array of characters so, a0 acts as an base address so reply has been loaded as its base address, a1 is set to 11 because we will be having a null character at the end which is to be omitted as per the project instructions. v0 is 8 as it is the convention for syscall read_string.

--- project1.s
+++ project1.s
@@ -4,3 +4,7 @@ reply:
 .space 10							        #Allowing 10 characters to input string
 main:	
 	li $s0, 0							#register to store sum of ASCII of valid values of the characters in our base system
+	li $v0, 8						
+	la $a0, reply							#Read input string
+	li $a1, 11
+	syscall
