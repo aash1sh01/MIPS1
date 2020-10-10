fcc014a7832f3772d1bd698836f7622863380f50
unknown <a.adhikari6960@gmail.com>
2020-10-10 10:59:10 +0545

    Here s0 has been initialised so that it can store the sum of values if they are valid in our base system.

--- project1.s
+++ project1.s
@@ -2,3 +2,5 @@
 												
 reply:	
 .space 10							        #Allowing 10 characters to input string
+main:	
+	li $s0, 0							#register to store sum of ASCII of valid values of the characters in our base system
