9d199c5edfb19b1a2fe6fea2defb84a8b3e4a221
unknown <a.adhikari6960@gmail.com>
2020-10-17 11:29:28 +0545

    Here the label, Last checks if the end of loop at s4 has been reached, that is, if 10 characters have been scanned, if so it will jump to End (which will have print syscall), else it will jump to Assign where each value is being read and validated through Checker

--- project1.s
+++ project1.s
@@ -12,3 +12,6 @@ main:
 	addi $s4, $s1, 10 						#Using 10 to $s4 to signify end of loop.
 Assign:	lb $a0, 0($s1)							
 	j Checker							#Load each character, with load byte and incerementing $s1 by 1 so that we jump to next character next, finally jmping to filter to check if the input is valid.
+Last:	beq $s4, $s1, End 						#Checking if 10 characters are done or not, if done, go to end of loop.
+	addi $s1, 1
+	j Assign							#increment address of reply by 1 until we've reached the end of the string
