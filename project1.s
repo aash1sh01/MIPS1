3ef97e4c1a09281200f005958dce9bd72d7773d3
unknown <a.adhikari6960@gmail.com>
2020-10-25 00:10:10 +0545

    I had made a blunderous mistake by missing .text which made me think for a long time as the program was not running but the individual snippets of code, like reading a string, printing an integer were all running, all my practice programs were running except the main program. Now after adding .text the main program is running.

--- project1.s
+++ project1.s
@@ -1,7 +1,10 @@
 .data
 												
 reply:	
-.space 10							        #Allowing 10 characters to input string
+.space 10								#Allowing 10 characters to input string
+
+.text
+							        
 main:	
 	li $s0, 0							#register to store sum of ASCII of valid values of the characters in our base system
 	li $v0, 8						
@@ -24,16 +27,17 @@ Checker: li $t1, 48							#checks if the values are valid and in range for our b
 	 li $t3, 64							#My Howard Id is 02986124, 02986124 mod 11= 9, and 26+9= 35, so base is 35 and the last valid ucase character is 'Y', hence $t4=89 , last valid lcase = 'y', hence $t6=121  
 	 li $t4, 89
 	 li $t5, 97
-	 li $t6, 121							#loading different values to registers to compare ASCII characters and filter if they are valid and also if the character is a number, lowercase or uppercase
+	 li $t6, 121
+									#loading different values to registers to compare ASCII characters and filter if they are valid and also if the character is a number, lowercase or uppercase
 	 blt $a0, $t1, Invalid											
 	 bgt $a0, $t6, Invalid 						#checking whether the values are beyond the range or not, if yes we will jump to invalid, if no we will jump to isValid
-	 blt $a0, $t6, isvalid
+	 ble $a0, $t6, isvalid
 	
-isvalid: bgt $a0, $t5, Lcase						#checking if characters are valid in our base system and categorizing them into individual branches by comparing stored ascii values.
+isvalid: bge $a0, $t5, Lcase						#checking if characters are valid in our base system and categorizing them into individual branches by comparing stored ascii values.
 	 bgt $a0, $t2, Invalid
 	 bgt $a0, $t3, Ucase
 	 bgt $a0, $t4, Invalid
-	 bgt $a0, $t1, integer
+	 bge $a0, $t1, integer
 
 integer: li $s2, -48							#Case 1: when character is integer
 	 add $s3, $a0, $s2						#if character is a numeric character
@@ -51,10 +55,12 @@ Ucase:	li $s2, -55							#Case 3: when the character is uppercase
 	j Last
 
 Invalid: add $s0, $s0, $zero
-	 j Last								#if invalid, we add zero to the total value then go to next character
+	 j Last	
+									#if invalid, we add zero to the total value then go to next character
 End:	 li $v0, 1
 	 add $a0, $s0, $zero						#print the sum value stored in $s0 across all three cases
-	 syscall	
+	 syscall
+	
 	 li $v0, 10							#end once the output is printed
 	 syscall
 					
